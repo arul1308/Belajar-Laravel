15a797ee82b99de3917a0799be3140dcef0c0aa4
Hypnotic 13 <Hypnotic>
2022-03-05 22:31:31 +0700

    Login User & Middleware

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
@@ -12,5 +13,21 @@ class RegisterController extends Controller
             'tittle' => 'Register',
             'active' => 'register'
         ]);
+    }
+    public function store(Request $request)
+    {
+        $validatedData = $request -> validate([
+            'name' => 'required|max:255',
+            'username' => 'required|min:4|max:16|unique:users',
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:8|max:255'
+        ]);
+
+        $validatedData['password'] = bcrypt($validatedData['password']);
+
+        User::create($validatedData);
+        //$request -> session()-> flash('success', 'Registration Success');
+        return redirect('/login') -> with ('success', 'Registration Success');
+
     }
 }
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -19,10 +19,29 @@
             <a class="nav-link {{ ($active === "posts") ? 'active' : '' }}" href="/categories">Genre</a>
           </li>
         </ul>
+        
+        
         <ul class="navbar-nav ms-auto">
-          <li class='nav-item'>
+          @auth
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Hello {{ auth()->user()->name }}
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-clipboard-fill"></i> Dashboard</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li>
+                <form action="/logout" method='post'>
+                  @csrf
+                  <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-right"></i> Logout</button>
+                </form>
+            </ul>
+          </li>
+          @else
+          <li class='nav-item' >
             <a href="/login" class="nav-link {{ ($active === "login") ? 'active' : '' }}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
           </li>
+          @endauth
         </ul>
       </div>
     </div>
