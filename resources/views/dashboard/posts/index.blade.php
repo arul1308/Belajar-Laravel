a51609a5f284267d971a26d389ba6ea773538db4
Hypnotic 13 <Hypnotic>
2022-03-08 23:05:40 +0700

    CRUD Bagian 2

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -5,6 +5,12 @@
     <h1 class="h2"> My Post </h1>
 </div>
 
+@if(session()->has('success'))
+  <div class="alert alert-success col-lg-10" role="alert">
+    {{ session('success') }}
+  </div>
+@endif
+
 <div class="table-responsive col-lg-10">
     <a href="/dashboard/posts/create" class="btn btn-primary"> Create New Post </a>
     <table class="table table-striped table-sm">
@@ -24,8 +30,12 @@
             <td>{{ $post -> category -> name }}</td>
             <td>
                 <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><span data-feather="eye"></span></a>
-                <a href="" class="badge bg-warning"><span data-feather="edit-3"></span></a>
-                <a href="" class="badge bg-danger"><span data-feather="trash-2"></span></a>
+                <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning"><span data-feather="edit-3"></span></a>
+                <form action="/dashboard/posts/{{ $post->slug }}" method=post class="d-inline">
+                  @method('delete')
+                  @csrf
+                  <button class="badge bg-danger border-0" onclick="return confirm('Are You Sure Want to Delete This Post?')"><span data-feather="trash-2"></span></button>
+                </form>
             </td>
           </tr>              
           @endforeach
--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -7,8 +7,12 @@
             <article>
                 <h2 class="mb-2">{{ $post->tittle }}</h2>
                 <a href="/dashboard/posts" class="btn btn-info "><span data-feather="arrow-left"></span> Back to my post</a>
-                <a href="" class="btn btn-warning"><span data-feather="edit-3"></span> Edit</a>
-                <a href="" class="btn btn-danger"><span data-feather="trash-2"></span> Delete</a>
+                <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit-3"></span> Edit</a>
+                <form action="/dashboard/posts/{{ $post->slug }}" method=post class="d-inline">
+                    @method('delete')
+                    @csrf
+                    <button class="btn btn-danger" onclick="return confirm('Are You Sure Want to Delete This Post?')"><span data-feather="trash-2"></span> Delete</button>
+                  </form>
                 <img src="https://source.unsplash.com/1200x500?"{{ $post->category->name }} alt="{{ $post->category->name }}" class="img-fluid mt-2">
                 <article class="my-3 fs-6">
                     
